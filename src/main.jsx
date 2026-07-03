1d0dd6163a7612f2c4188b492352641c23635e07
TY-HirokiNakano <hirokiiass@gmail.com>
2026-07-03 11:57:39 +0900

    route基礎の作成

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Router } from "react-router";
-import RoutesApp from "./components/RoutesApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <RoutesApp />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
