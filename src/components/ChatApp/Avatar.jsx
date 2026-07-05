ea438e081987172a9b7c928d416066b765199d2b
TY-HirokiNakano <hirokiiass@gmail.com>
2026-07-06 08:47:02 +0900

    仮アイコン画像設定

--- src/components/ChatApp/Avatar.jsx
+++ src/components/ChatApp/Avatar.jsx
@@ -2,9 +2,14 @@ export function Avatar({ post }) {
   return (
     <>
       {post === "GOD" ? (
-        <p style={{ backgroundColor: "gold" }}>GODアイコン</p>
+        <img src="/icon_god.png" alt="GODアイコン" width={150} height={150} />
       ) : (
-        <p style={{ backgroundColor: "grey" }}>userアイコン</p>
+        <img
+          src="/kkrn_icon_user_1.png"
+          alt="userアイコン"
+          width={50}
+          height={50}
+        />
       )}
     </>
   );
