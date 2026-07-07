b828a2636a62dd13771278d0f1c9ff22e65c62d8
KosukeHiramatsu <kosuke.h92@gmail.com>
2026-07-07 10:56:47 +0900

    ナビバーの実装（仮）

---
+++ front/src/components/Header.jsx
@@ -0,0 +1,102 @@
+import {
+  AppBar,
+  alpha,
+  Box,
+  Button,
+  Toolbar,
+  Typography,
+  TextField,
+  Dialog,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  DialogContent,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+
+import AlbumIcon from "@mui/icons-material/Album";
+import { useRef, useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+
+export function Header({}) {
+  //   const [link, setLink] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const authState = false;
+  //   const signOut = async () => {
+  //     const reqSignOut = await fetch("/api/firebase/signOut");
+  //     const json = await reqSignOut.json();
+  //   };
+
+  //   useEffect(() => {
+  //     //今ログインしてるユーザーの情報取得
+  //     const getuserId = async () => {
+  //       try {
+  //         const response = await fetch("api/user");
+  //         const json = await response.json();
+  //         setUserId(json.data.id);
+  //       } catch {
+  //         console.error("error");
+  //       }
+  //     };
+  //     getuserId();
+  //   });
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 0,
+        mb: "32px",
+        width: "100%",
+      }}
+    >
+      <AppBar
+        position="static"
+        sx={{
+          height: 60,
+          backgroundColor: alpha("#ffffff", 0.5),
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            aria-label="menu"
+            sx={{ ml: 1, mr: 2 }}
+            // onClick={() => {
+            //   if (userId) {
+            //     setLink("/team");
+            //   }
+            // }}
+            // href={link}
+          >
+            <AlbumIcon color="primary" sx={{ mr: 2 }} />
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              color="primary"
+              fontWeight="bold"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              GOD
+            </Typography>
+          </IconButton>
+          <Button
+            variant="outlined"
+            // onClick={signOut}
+            href="/"
+            sx={{
+              mr: 2,
+            }}
+          >
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- front/src/components/RoutesApp.jsx
+++ front/src/components/RoutesApp.jsx
@@ -2,9 +2,12 @@ import { Route, Routes } from "react-router";
 import { LoginApp } from "./LoginApp/LoginApp";
 import { ChatApp } from "./ChatApp/ChatApp";
 import { ProductDetail } from "./ProductDetail/ProductDetail";
+import { Header } from "./Header";
+
 function RoutesApp() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<LoginApp />} />
         <Route path="/chat" element={<ChatApp />} />
