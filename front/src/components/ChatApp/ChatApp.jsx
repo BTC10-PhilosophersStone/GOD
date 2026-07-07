37285309d54e3cfd8e4386d066c13ddea7d978cf
TY-HirokiNakano <hirokiiass@gmail.com>
2026-07-07 16:13:11 +0900

    chatHeader削除

--- front/src/components/ChatApp/ChatApp.jsx
+++ front/src/components/ChatApp/ChatApp.jsx
@@ -44,7 +44,6 @@ export function ChatApp() {
       <h1>チャット画面</h1>
       {/* <Link to="/">ログイン画面に戻る</Link>
       <Link to="/product">product詳細</Link> */}
-      <ChatHeader />
       <MessageList messageList={messageList} />
       <Prompt />
 
