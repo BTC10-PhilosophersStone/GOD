10dbf97575e22207b348eea107a6cf2b3dd7f4f2
makoti <takaomi_mako@mail.toyota.co.jp>
2026-07-08 10:56:31 +0900

    バックエンド合体3

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -23,7 +23,6 @@ export function Header({}) {
   //   const [link, setLink] = useState(null);
   const [userId, setUserId] = useState(null);
   const authState = false;
-  console.log("authState");
   //   const signOut = async () => {
   //     const reqSignOut = await fetch("/api/firebase/signOut");
   //     const json = await reqSignOut.json();
