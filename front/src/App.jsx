2ddbe5947242fc08c2b244cc26cd6d1cfebfee15
makoti <takaomi_mako@mail.toyota.co.jp>
2026-07-13 15:46:33 +0900

    一覧表示画面完了

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,13 @@
 import RoutesApp from "./components/RoutesApp";
 import Modal from "react-modal";
 import { ListView } from "./components/ListView/ListView";
+import { ChatApp } from "./components/ChatApp/ChatApp";
 Modal.setAppElement("#root");
 
 function App() {
   return (
     <>
-      <ListView />
+      <ChatApp />
     </>
   );
 }
