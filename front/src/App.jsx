3ccd32712159cc139dfad9c7fe2ed38f7ec90b49
makoti <takaomi_mako@mail.toyota.co.jp>
2026-07-13 15:44:34 +0900

    画面完了

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,5 @@
 import RoutesApp from "./components/RoutesApp";
 import Modal from "react-modal";
-import Text from "./components/SearchListOld/Text";
-import SearchList from "./components/SearchListOld/SearchList";
 import { ListView } from "./components/ListView/ListView";
 Modal.setAppElement("#root");
 
